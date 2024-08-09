a0a695ac4c7bc54108eff7baf1e8da72d3ebeab4
claustra01 <shinya430.g@gmail.com>
2024-08-10 02:19:45 +0900

    fix: visualizer ci

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), visualizer({filename: 'dist/stats.html'}),],
+  plugins: [react(), visualizer({filename: 'dist/visualizer/index.html'}),],
 })
