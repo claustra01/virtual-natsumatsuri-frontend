949b42c846bfdc0c61acc569a7ac48b08aaa741e
claustra01 <shinya430.g@gmail.com>
2024-08-10 02:15:24 +0900

    add: visualizer ci

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
+import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), visualizer({filename: 'dist/stats.html'}),],
 })
