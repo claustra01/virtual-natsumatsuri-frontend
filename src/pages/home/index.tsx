5d6b8f874bdde7882b5efea76bd4eea623813368
Sea10wood <miyusea5@gmail.com>
2024-08-11 02:17:17 +0900

    fix:homeからHomeSPとHomePCヘの切り分け

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
-import { DefaultButton } from "../../components/ui/Button";
 import { device } from "../../utils/device";
-import styles from "./index.module.css";
+import HomePC from "../homePC/index.tsx";
+import HomeSP from "../homeSP/index.tsx";
 
 function Home() {
 	const [isPcScreen, setIsPcScreen] = useState(
@@ -11,9 +11,7 @@ function Home() {
 	useEffect(() => {
 		const mediaQuery = window.matchMedia(device.pc);
 
-		const handleChange = (event: {
-			matches: boolean | ((prevState: boolean) => boolean);
-		}) => {
+		const handleChange = (event: { matches: boolean }) => {
 			setIsPcScreen(event.matches);
 		};
 
@@ -23,57 +21,7 @@ function Home() {
 		};
 	}, []);
 
-	const handleClick = () => {
-		const audio = new Audio("/sound/wadaiko.mp3");
-		audio
-			.play()
-			.then(() => {
-				setTimeout(() => {
-					window.location.href = isPcScreen ? "/yatai" : "/shooter";
-				}, 500);
-			})
-			.catch((error) => {
-				console.error("オーディオの音が出なかった", error);
-				window.location.href = isPcScreen ? "/yatai" : "/shooter";
-			});
-	};
-
-	return (
-		<div>
-			<div className={styles["scroll-infinity"]}>
-				<div className={styles["scroll-infinity__wrap"]}>
-					<ul
-						className={`${styles["scroll-infinity__list"]} ${styles["scroll-infinity__list--left"]}`}
-					>
-						<li className={styles["scroll-infinity__item"]}>
-							VIRTUAL_NATSUMATSURI
-						</li>
-						<li className={styles["scroll-infinity__item"]}>
-							VIRTUAL_NATSUMATSURI
-						</li>
-						<li className={styles["scroll-infinity__item"]}>
-							VIRTUAL_NATSUMATSURI
-						</li>
-					</ul>
-				</div>
-			</div>
-			<div className={styles["background-logo"]}>
-				<img
-					src="/logo.webp"
-					alt="背景にばーちゃるなつまつりのロゴ"
-					width="350"
-					height="350"
-				/>
-			</div>
-			<div
-				className={`${styles["go-game"]} ${isPcScreen ? styles["go-game-pc"] : ""}`}
-			>
-				<DefaultButton color="red" size="lg" onClick={handleClick}>
-					射的へ向かう
-				</DefaultButton>
-			</div>
-		</div>
-	);
+	return isPcScreen ? <HomePC /> : <HomeSP />;
 }
 
 export default Home;
---
+++ src/pages/homeSP/index.tsx
@@ -0,0 +1,58 @@
+import { DefaultButton } from "../../components/ui/Button";
+import styles from "./index.module.css";
+
+function HomeSP() {
+	const handleClick = () => {
+		const audio = new Audio("/sound/wadaiko.mp3");
+		audio
+			.play()
+			.then(() => {
+				setTimeout(() => {
+					window.location.href = "/shooter";
+				}, 500);
+			})
+			.catch((error) => {
+				console.error("オーディオの音が出なかった", error);
+				window.location.href = "/shooter";
+			});
+	};
+
+	return (
+		<div>
+			<div className={styles.container}>
+				<div className={styles["scroll-infinity"]}>
+					<div className={styles["scroll-infinity__wrap"]}>
+						<ul
+							className={`${styles["scroll-infinity__list"]} ${styles["scroll-infinity__list--left"]}`}
+						>
+							<li className={styles["scroll-infinity__item"]}>
+								VIRTUAL_NATSUMATSURI
+							</li>
+							<li className={styles["scroll-infinity__item"]}>
+								VIRTUAL_NATSUMATSURI
+							</li>
+							<li className={styles["scroll-infinity__item"]}>
+								VIRTUAL_NATSUMATSURI
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div className={styles["background-logo"]}>
+					<img
+						src="/logo.webp"
+						alt="背景にばーちゃるなつまつりのロゴ"
+						width="380"
+						height="380"
+					/>
+				</div>
+				<div className={styles["go-game-sp"]}>
+					<DefaultButton color="red" size="lg" onClick={handleClick}>
+						射的へ向かう
+					</DefaultButton>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default HomeSP;
