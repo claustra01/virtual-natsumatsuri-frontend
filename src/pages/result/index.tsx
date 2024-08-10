e60111b8d3dc817c553da33ab7d90cad0a73bb4e
Sea10wood <miyusea5@gmail.com>
2024-08-11 07:44:08 +0900

    Add: result_pageのview

---
+++ src/pages/result/index.tsx
@@ -0,0 +1,13 @@
+import styles from "./index.module.css";
+
+function Result() {
+	return (
+		<div>
+			<div className={styles["result-text"]}>
+				<p>結果発表!</p>
+			</div>
+		</div>
+	);
+}
+
+export default Result;
