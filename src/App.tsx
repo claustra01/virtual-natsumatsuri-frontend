8a380f59ad0cdd0c519c9ce2032d23a1c3f38e58
Sea10wood <miyusea5@gmail.com>
2024-08-10 18:15:49 +0900

    add: ShooterButtonPage、galleryをyataiに変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
+import Shooter from "./pages/shooter";
 const AppRoutes = () => {
 	return (
 		<>
 			<Routes>
 				<Route path="/" element={<Home />} />
+				<Route path="/shooter" element={<Shooter />} />
 			</Routes>
 		</>
 	);
