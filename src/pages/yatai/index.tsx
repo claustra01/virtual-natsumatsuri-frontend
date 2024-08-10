21b341a42c0cad74058163976bed716cebeb1cb9
claustra01 <shinya430.g@gmail.com>
2024-08-10 18:07:57 +0900

    add: shooting yatai

--- src/pages/yatai/index.tsx
+++ src/pages/yatai/index.tsx
@@ -1,35 +1,63 @@
-import * as THREE from 'three'
-import { useRef, useState } from 'react'
-import { Canvas, useFrame, ThreeElements } from '@react-three/fiber'
+import { Canvas, useThree, type ThreeElements } from "@react-three/fiber";
+import styles from "./index.module.css";
+
+const Foundation = (props: ThreeElements["mesh"]) => {
+  return (
+    <mesh
+      {...props}
+      castShadow
+      receiveShadow
+    >
+      <boxGeometry args={[10, 2, 2]} />
+      <meshStandardMaterial color={"red"} />
+    </mesh>
+  );
+}
+
+const Target = (props: ThreeElements["mesh"]) => {
+  return (
+    <mesh
+      {...props}
+      castShadow
+      receiveShadow
+    >
+      <boxGeometry args={[0.7, 2, 0.7]} />
+      <meshStandardMaterial color={"yellow"} />
+    </mesh>
+  );
+}
+
+const CameraController = () => {
+  const { camera } = useThree();
+  camera.position.set(0, 3, 12);
+  camera.lookAt(0, 2, 0);
+  return null;
+}
 
 function Yatai() {
-  const Box = (props: ThreeElements['mesh']) => {
-    const meshRef = useRef<THREE.Mesh>(null!)
-    const [hovered, setHover] = useState(false)
-    const [active, setActive] = useState(false)
-    useFrame((state, delta) => (meshRef.current.rotation.x += delta))
-    return (
-      <mesh
-        {...props}
-        ref={meshRef}
-        scale={active ? 1.5 : 1}
-        onClick={(event) => setActive(!active)}
-        onPointerOver={(event) => setHover(true)}
-        onPointerOut={(event) => setHover(false)}>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-      </mesh>
-    )
-  }
-
   return (
-    <Canvas>
-      <ambientLight intensity={Math.PI / 2} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
-    </Canvas>
+    <div className={styles["canvas"]}>
+      <Canvas shadows camera={{ fov: 25 }}>
+        <ambientLight intensity={Math.PI / 16} />
+        <spotLight 
+          castShadow 
+          position={[4, 12, 8]} 
+          angle={0.5} 
+          penumbra={0}
+          decay={0}
+          intensity={Math.PI}
+          shadow-mapSize-width={1024} 
+          shadow-mapSize-height={1024} 
+          shadow-bias={-0.0001}
+        />
+        <CameraController />
+        <Foundation position={[0, 2, -2]} />
+        <Foundation position={[0, 0, 0]} />
+        <Target position={[-3, 1.8, 0]} />
+        <Target position={[0, 1.8, 0]} />
+        <Target position={[3, 1.8, 0]} />
+      </Canvas>
+    </div>
   );
 }
 
