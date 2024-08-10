578b63e8fa1cc644175ae6995cdc6e5d73d2d769
K-Kizuku <kotani.kizuku@gmail.com>
2024-08-11 02:07:19 +0900

    fix: lint

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -6,85 +6,85 @@ import { requestPermission } from "../../utils/parmission";
 import styles from "./index.module.css";
 
 function Home() {
-  const [isPcScreen, setIsPcScreen] = useState(
-    window.matchMedia(device.pc).matches
-  );
-  const setRef = useSocketRefStore((state) => state.setRef);
+	const [isPcScreen, setIsPcScreen] = useState(
+		window.matchMedia(device.pc).matches,
+	);
+	const setRef = useSocketRefStore((state) => state.setRef);
 
-  useEffect(() => {
-    const mediaQuery = window.matchMedia(device.pc);
+	useEffect(() => {
+		const mediaQuery = window.matchMedia(device.pc);
 
-    const handleChange = (event: {
-      matches: boolean | ((prevState: boolean) => boolean);
-    }) => {
-      setIsPcScreen(event.matches);
-    };
+		const handleChange = (event: {
+			matches: boolean | ((prevState: boolean) => boolean);
+		}) => {
+			setIsPcScreen(event.matches);
+		};
 
-    mediaQuery.addEventListener("change", handleChange);
-    return () => {
-      mediaQuery.removeEventListener("change", handleChange);
-    };
-  }, []);
+		mediaQuery.addEventListener("change", handleChange);
+		return () => {
+			mediaQuery.removeEventListener("change", handleChange);
+		};
+	}, []);
 
-  const handleClick = () => {
-    const audio = new Audio("/sound/wadaiko.mp3");
-    audio
-      .play()
-      .then(() => {
-        setTimeout(() => {
-          window.location.href = isPcScreen ? "/yatai" : "/shooter";
-        }, 500);
-      })
-      .catch((error) => {
-        console.error("オーディオの音が出なかった", error);
-        window.location.href = isPcScreen ? "/yatai" : "/shooter";
-      });
-    requestPermission();
-    const socketRef = new WebSocket(
-      `wss://${import.meta.env.VITE_HOST_NAME}/ws`
-    );
-    setRef({ current: socketRef });
-    window.location.href = isPcScreen ? "/yatai" : "/shooter";
-  };
+	const handleClick = () => {
+		const audio = new Audio("/sound/wadaiko.mp3");
+		audio
+			.play()
+			.then(() => {
+				setTimeout(() => {
+					window.location.href = isPcScreen ? "/yatai" : "/shooter";
+				}, 500);
+			})
+			.catch((error) => {
+				console.error("オーディオの音が出なかった", error);
+				window.location.href = isPcScreen ? "/yatai" : "/shooter";
+			});
+		requestPermission();
+		const socketRef = new WebSocket(
+			`wss://${import.meta.env.VITE_HOST_NAME}/ws`,
+		);
+		setRef({ current: socketRef });
+		window.location.href = isPcScreen ? "/yatai" : "/shooter";
+	};
 
-  return (
-    <div>
-      <div className={styles["scroll-infinity"]}>
-        <div className={styles["scroll-infinity__wrap"]}>
-          <ul
-            className={`${styles["scroll-infinity__list"]} ${styles["scroll-infinity__list--left"]}`}
-          >
-            <li className={styles["scroll-infinity__item"]}>
-              VIRTUAL_NATSUMATSURI
-            </li>
-            <li className={styles["scroll-infinity__item"]}>
-              VIRTUAL_NATSUMATSURI
-            </li>
-            <li className={styles["scroll-infinity__item"]}>
-              VIRTUAL_NATSUMATSURI
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div className={styles["background-logo"]}>
-        <img
-          src="/logo.webp"
-          alt="背景にばーちゃるなつまつりのロゴ"
-          width="350"
-          height="350"
-        />
-      </div>
-      <div
-        className={`${styles["go-game"]} ${
-          isPcScreen ? styles["go-game-pc"] : ""
-        }`}
-      >
-        <DefaultButton color="red" size="lg" onClick={handleClick}>
-          射的へ向かう
-        </DefaultButton>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div className={styles["scroll-infinity"]}>
+				<div className={styles["scroll-infinity__wrap"]}>
+					<ul
+						className={`${styles["scroll-infinity__list"]} ${styles["scroll-infinity__list--left"]}`}
+					>
+						<li className={styles["scroll-infinity__item"]}>
+							VIRTUAL_NATSUMATSURI
+						</li>
+						<li className={styles["scroll-infinity__item"]}>
+							VIRTUAL_NATSUMATSURI
+						</li>
+						<li className={styles["scroll-infinity__item"]}>
+							VIRTUAL_NATSUMATSURI
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div className={styles["background-logo"]}>
+				<img
+					src="/logo.webp"
+					alt="背景にばーちゃるなつまつりのロゴ"
+					width="350"
+					height="350"
+				/>
+			</div>
+			<div
+				className={`${styles["go-game"]} ${
+					isPcScreen ? styles["go-game-pc"] : ""
+				}`}
+			>
+				<DefaultButton color="red" size="lg" onClick={handleClick}>
+					射的へ向かう
+				</DefaultButton>
+			</div>
+		</div>
+	);
 }
 
 export default Home;
--- src/pages/shooter/index.tsx
+++ src/pages/shooter/index.tsx
@@ -8,109 +8,109 @@ import { type Schema, message_type } from "../../type/schema";
 import style from "./index.module.css";
 
 const Shooter = () => {
-  const [isOpen, setIsOpen] = useState(true);
-  const { orientationDiff } = useOrientation();
-  const socketRef = useSocketRefStore((state) => state.socketRef);
+	const [isOpen, setIsOpen] = useState(true);
+	const { orientationDiff } = useOrientation();
+	const socketRef = useSocketRefStore((state) => state.socketRef);
 
-  const initialImages = [
-    "/2D_material/cork.webp",
-    "/2D_material/cork.webp",
-    "/2D_material/cork.webp",
-  ];
+	const initialImages = [
+		"/2D_material/cork.webp",
+		"/2D_material/cork.webp",
+		"/2D_material/cork.webp",
+	];
 
-  const [images, setImages] = useState(initialImages);
+	const [images, setImages] = useState(initialImages);
 
-  const handleClick = () => {
-    const audio = new Audio("/sound/cork_sound.mp3");
-    audio
-      .play()
-      .then(() => {})
-      .catch((error) => {
-        console.error("オーディオの音が出なかった", error);
-      });
-    const data: Schema = {
-      id: "shooter",
-      interval: 0,
-      angle: {
-        x: orientationDiff.beta,
-        y: orientationDiff.gamma,
-      },
-      acceleration: {
-        x: 0,
-        y: 0,
-        z: 0,
-      },
-      distance: {
-        x: 0,
-        y: 0,
-        z: 0,
-      },
-      message_type: message_type.action,
-    };
-    socketRef?.current?.send(JSON.stringify(data));
-    setImages((prevImages) => prevImages.slice(1));
-  };
+	const handleClick = () => {
+		const audio = new Audio("/sound/cork_sound.mp3");
+		audio
+			.play()
+			.then(() => {})
+			.catch((error) => {
+				console.error("オーディオの音が出なかった", error);
+			});
+		const data: Schema = {
+			id: "shooter",
+			interval: 0,
+			angle: {
+				x: orientationDiff.beta,
+				y: orientationDiff.gamma,
+			},
+			acceleration: {
+				x: 0,
+				y: 0,
+				z: 0,
+			},
+			distance: {
+				x: 0,
+				y: 0,
+				z: 0,
+			},
+			message_type: message_type.action,
+		};
+		socketRef?.current?.send(JSON.stringify(data));
+		setImages((prevImages) => prevImages.slice(1));
+	};
 
-  const handleKeyUp: KeyboardEventHandler<HTMLDivElement> = (event) => {
-    if (event.key === "Enter" || event.key === " ") {
-      handleClick();
-    }
-  };
+	const handleKeyUp: KeyboardEventHandler<HTMLDivElement> = (event) => {
+		if (event.key === "Enter" || event.key === " ") {
+			handleClick();
+		}
+	};
 
-  return (
-    <div>
-      <Modal open={isOpen} onClose={() => setIsOpen(false)}>
-        <ModalContent setIsOpen={setIsOpen} />
-      </Modal>
-      <div className={style.trigger}>
-        <ShooterButton onClick={handleClick} onKeyUp={handleKeyUp} />
-      </div>
-      <div className={style.cork}>
-        {images.map((src, i) => (
-          // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
-          <img key={i} src={src} alt="コルクの残量を表示しています" />
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<Modal open={isOpen} onClose={() => setIsOpen(false)}>
+				<ModalContent setIsOpen={setIsOpen} />
+			</Modal>
+			<div className={style.trigger}>
+				<ShooterButton onClick={handleClick} onKeyUp={handleKeyUp} />
+			</div>
+			<div className={style.cork}>
+				{images.map((src, i) => (
+					// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+					<img key={i} src={src} alt="コルクの残量を表示しています" />
+				))}
+			</div>
+		</div>
+	);
 };
 
 type ModalContentProps = {
-  setIsOpen: (isOpen: boolean) => void;
+	setIsOpen: (isOpen: boolean) => void;
 };
 
 const ModalContent: React.FC<ModalContentProps> = ({ setIsOpen }) => {
-  const { reset } = useOrientation();
-  return (
-    <div className={style["modal-wrapper"]}>
-      <img
-        src="/2D_material/modal.webp"
-        alt="スマホを画面に向かって垂直におく図"
-        width="100"
-        height="100"
-      />
-      <div className={style["modal-row"]}>
-        <p className={style["modal-description"]}>
-          スマホを画面に向かって
-          <br />
-          垂直に机の上に置いてね
-        </p>
-      </div>
-      <div className={style["modal-selection-wrapper"]}>
-        <DefaultButton
-          variant="outlined"
-          color="red"
-          size="md"
-          onClick={() => {
-            reset();
-            setIsOpen(false);
-          }}
-        >
-          置いたよ！
-        </DefaultButton>
-      </div>
-    </div>
-  );
+	const { reset } = useOrientation();
+	return (
+		<div className={style["modal-wrapper"]}>
+			<img
+				src="/2D_material/modal.webp"
+				alt="スマホを画面に向かって垂直におく図"
+				width="100"
+				height="100"
+			/>
+			<div className={style["modal-row"]}>
+				<p className={style["modal-description"]}>
+					スマホを画面に向かって
+					<br />
+					垂直に机の上に置いてね
+				</p>
+			</div>
+			<div className={style["modal-selection-wrapper"]}>
+				<DefaultButton
+					variant="outlined"
+					color="red"
+					size="md"
+					onClick={() => {
+						reset();
+						setIsOpen(false);
+					}}
+				>
+					置いたよ！
+				</DefaultButton>
+			</div>
+		</div>
+	);
 };
 
 export default Shooter;
