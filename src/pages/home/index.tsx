73fcab0928084620ffb8d8585a8d47e71fb8631e
claustra01 <shinya430.g@gmail.com>
2024-08-11 08:24:56 +0900

    fix: build

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,20 +1,12 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import HomePC from "../../components/responsive/homePC";
 import HomeSP from "../../components/responsive/homeSP";
-import { DefaultButton } from "../../components/ui/Button";
-import { useSocketRefStore } from "../../store";
 import { device } from "../../utils/device";
-import { requestPermission } from "../../utils/permission";
-import styles from "./index.module.css";
 
 function Home() {
-	const navigate = useNavigate();
-
 	const [isPcScreen, setIsPcScreen] = useState(
 		window.matchMedia(device.pc).matches,
 	);
-	const setRef = useSocketRefStore((state) => state.setRef);
 
 	useEffect(() => {
 		const mediaQuery = window.matchMedia(device.pc);
