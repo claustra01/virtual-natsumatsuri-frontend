1ca184078407dd8fc57cb2cd973328516b89eac0
Sea10wood <miyusea5@gmail.com>
2024-08-10 21:57:28 +0900

    Add: スマホを画面に向かって垂直に机の上に置いてねモーダルの実装

--- src/pages/shooter/index.tsx
+++ src/pages/shooter/index.tsx
@@ -1,8 +1,12 @@
 import { type KeyboardEventHandler, useState } from "react";
+import { DefaultButton } from "../../components/ui/Button";
+import { Modal } from "../../components/ui/Modal";
 import { ShooterButton } from "../../components/ui/ShooterButton";
 import style from "./index.module.css";
 
 function Shooter() {
+	const [isOpen, setIsOpen] = useState(true);
+
 	const initialImages = [
 		"/2D_material/cork.webp",
 		"/2D_material/cork.webp",
@@ -23,13 +27,11 @@ function Shooter() {
 
 	return (
 		<div>
+			<Modal open={isOpen} onClose={() => setIsOpen(false)}>
+				<ModalContent setIsOpen={setIsOpen} />
+			</Modal>
 			<div className={style.trigger}>
-				<ShooterButton
-					onClick={handleClick}
-					onKeyUp={handleKeyUp}
-					onKeyDown={handleKeyUp}
-					onKeyPress={handleKeyUp}
-				/>
+				<ShooterButton onClick={handleClick} onKeyUp={handleKeyUp} />
 			</div>
 			<div className={style.cork}>
 				{images.map((src) => (
@@ -39,5 +41,39 @@ function Shooter() {
 		</div>
 	);
 }
+
+type ModalContentProps = {
+	setIsOpen: (isOpen: boolean) => void;
+};
+
+const ModalContent: React.FC<ModalContentProps> = ({ setIsOpen }) => {
+	return (
+		<div className={style["modal-wrapper"]}>
+			<img
+				src="/2D_material/modal.webp"
+				alt="スマホを画面に向かって垂直におく図"
+				width="100"
+				height="100"
+			/>
+			<div className={style["modal-row"]}>
+				<p className={style["modal-description"]}>
+					スマホを画面に向かって
+					<br />
+					垂直に机の上に置いてね
+				</p>
+			</div>
+			<div className={style["modal-selection-wrapper"]}>
+				<DefaultButton
+					variant="outlined"
+					color="red"
+					size="md"
+					onClick={() => setIsOpen(false)}
+				>
+					置いたよ！
+				</DefaultButton>
+			</div>
+		</div>
+	);
+};
 
 export default Shooter;
