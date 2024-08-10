c37c91de32cbae979171bc122bc840087df8de86
claustra01 <shinya430.g@gmail.com>
2024-08-10 22:40:24 +0900

    wip: cannon engine

--- src/pages/yatai/index.tsx
+++ src/pages/yatai/index.tsx
@@ -1,6 +1,15 @@
+import { Physics, useBox } from "@react-three/cannon";
 import { Canvas, type ThreeElements, useThree } from "@react-three/fiber";
-import styles from "./index.module.css";
 import { useState } from "react";
+import {
+	type BufferGeometry,
+	type Material,
+	type Mesh,
+	type NormalBufferAttributes,
+	type Object3DEventMap,
+	Vector3,
+} from "three";
+import styles from "./index.module.css";
 
 function Yatai() {
 	// 土台
@@ -17,8 +26,29 @@ function Yatai() {
 	const Target = (props: ThreeElements["mesh"]) => {
 		// 一旦ホバーでアクションを起こす
 		const [isActive, setIsActive] = useState<boolean>(false);
+		const [ref] = useBox(() => ({
+			mass: 1,
+			position:
+				props.position instanceof Vector3
+					? props.position.toArray()
+					: new Vector3().toArray(),
+		}));
 		return (
-			<mesh {...props} castShadow receiveShadow onPointerOver={() => setIsActive(!isActive)}>
+			<mesh
+				ref={
+					ref as React.Ref<
+						Mesh<
+							BufferGeometry<NormalBufferAttributes>,
+							Material | Material[],
+							Object3DEventMap
+						>
+					>
+				}
+				{...props}
+				castShadow
+				receiveShadow
+				onPointerOver={() => setIsActive(!isActive)}
+			>
 				<boxGeometry args={[0.7, 2, 0.7]} />
 				<meshStandardMaterial color={isActive ? "black" : "yellow"} />
 			</mesh>
@@ -35,26 +65,28 @@ function Yatai() {
 	return (
 		<div className={styles.canvas}>
 			<Canvas shadows camera={{ fov: 25 }}>
-				{/* 全体ライト */}
-				<ambientLight intensity={Math.PI / 16} />
-				{/* スポットライト */}
-				<spotLight
-					castShadow
-					position={[4, 12, 8]}
-					angle={0.5}
-					penumbra={0}
-					decay={0}
-					intensity={Math.PI}
-					shadow-mapSize-width={1024}
-					shadow-mapSize-height={1024}
-					shadow-bias={-0.0001}
-				/>
-				<CameraController />
-				<Foundation position={[0, 2, -2]} />
-				<Foundation position={[0, 0, 0]} />
-				<Target position={[-3, 1.8, 0]} />
-				<Target position={[0, 1.8, 0]} />
-				<Target position={[3, 1.8, 0]} />
+				<Physics>
+					{/* 全体ライト */}
+					<ambientLight intensity={Math.PI / 16} />
+					{/* スポットライト */}
+					<spotLight
+						castShadow
+						position={[4, 12, 8]}
+						angle={0.5}
+						penumbra={0}
+						decay={0}
+						intensity={Math.PI}
+						shadow-mapSize-width={1024}
+						shadow-mapSize-height={1024}
+						shadow-bias={-0.0001}
+					/>
+					<CameraController />
+					<Foundation position={[0, 2, -2]} />
+					<Foundation position={[0, 0, 0]} />
+					<Target position={[-3, 1.8, 0]} />
+					<Target position={[0, 1.8, 0]} />
+					<Target position={[3, 1.8, 0]} />
+				</Physics>
 			</Canvas>
 		</div>
 	);
