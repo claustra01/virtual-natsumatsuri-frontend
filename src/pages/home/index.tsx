af47151703f2252bb27d0cb237e90c5697885314
Sea10wood <miyusea5@gmail.com>
2024-08-10 15:12:38 +0900

    Add: スマホ版ののHomeのUI（メディアクエリは夜眠くなってからに回す）

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,22 +1,39 @@
+import { DefaultButton } from "../../components/ui/Button";
 import styles from "./index.module.css";
 
 function Home() {
 	return (
-		<div className={styles["scroll-infinity"]}>
-			<div className={styles["scroll-infinity__wrap"]}>
-				<ul
-					className={`${styles["scroll-infinity__list"]} ${styles["scroll-infinity__list--left"]}`}
-				>
-					<li className={styles["scroll-infinity__item"]}>
-						VIRTUAL_NATSUMATSURI
-					</li>
-					<li className={styles["scroll-infinity__item"]}>
-						VIRTUAL_NATSUMATSURI
-					</li>
-					<li className={styles["scroll-infinity__item"]}>
-						VIRTUAL_NATSUMATSURI
-					</li>
-				</ul>
+    
+		<div>
+			<div className={styles["scroll-infinity"]}>
+				<div className={styles["scroll-infinity__wrap"]}>
+					<ul
+						className={`${styles["scroll-infinity__list"]} ${styles["scroll-infinity__list--left"]}`}
+					>
+						<li className={styles["scroll-infinity__item"]}>
+							VIRTUAL_NATSUMATSURI
+						</li>
+						<li className={styles["scroll-infinity__item"]}>
+							VIRTUAL_NATSUMATSURI
+						</li>
+						<li className={styles["scroll-infinity__item"]}>
+							VIRTUAL_NATSUMATSURI
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div className={styles["background-logo"]}>
+				<img
+					src="/logo.webp"
+					alt="背景にばーちゃるなつまつりのロゴ"
+					width="350"
+					height="350"
+				/>
+			</div>
+			<div className={styles["go-game"]}>
+				<DefaultButton color="red" size="lg">
+					射的へ向かう
+				</DefaultButton>
 			</div>
 		</div>
 	);
