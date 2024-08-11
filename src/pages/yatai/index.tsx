258dbdf60da66d3f5f6f2c3a01d98c378dd3094b
claustra01 <shinya430.g@gmail.com>
2024-08-11 09:47:38 +0900

    add: aim target render layer

--- src/pages/yatai/index.tsx
+++ src/pages/yatai/index.tsx
@@ -43,7 +43,7 @@ function Yatai() {
 	const [target, setTarget] = useState<Target | undefined>(undefined);
 	const shotTarget = (data: ActionSchema) => {
 		setTarget(data.target);
-	}
+	};
 
 	// 土台
 	const Foundation = (props: ThreeElements["mesh"]) => {
@@ -121,31 +121,48 @@ function Yatai() {
 	};
 
 	return (
-		<div className={styles.canvas}>
-			<Canvas shadows camera={{ fov: 25 }}>
-				<Physics>
-					{/* 全体ライト */}
-					<ambientLight intensity={Math.PI / 16} />
-					{/* スポットライト */}
-					<spotLight
-						castShadow
-						position={[4, 12, 8]}
-						angle={0.5}
-						penumbra={0}
-						decay={0}
-						intensity={Math.PI}
-						shadow-mapSize-width={1024}
-						shadow-mapSize-height={1024}
-						shadow-bias={-0.0001}
-					/>
-					<CameraController />
-					<Foundation position={[0, 2, -2]} />
-					<Foundation position={[0, 0, 0]} />
-					<Target position={[-3, 1.8, 0]} />
-					<Target position={[0, 1.8, 0]} />
-					<Target position={[3, 1.8, 0]} />
-				</Physics>
-			</Canvas>
+		<div className={styles.container}>
+			<div className={styles.canvas}>
+				<Canvas shadows camera={{ fov: 25 }}>
+					<Physics>
+						{/* 全体ライト */}
+						<ambientLight intensity={Math.PI / 16} />
+						{/* スポットライト */}
+						<spotLight
+							castShadow
+							position={[4, 12, 8]}
+							angle={0.5}
+							penumbra={0}
+							decay={0}
+							intensity={Math.PI}
+							shadow-mapSize-width={1024}
+							shadow-mapSize-height={1024}
+							shadow-bias={-0.0001}
+						/>
+						<CameraController />
+						<Foundation position={[0, 2, -2]} />
+						<Foundation position={[0, 0, 0]} />
+						<Target position={[-3, 1.8, 0]} />
+						<Target position={[0, 1.8, 0]} />
+						<Target position={[3, 1.8, 0]} />
+					</Physics>
+				</Canvas>
+			</div>
+			<div
+				className={styles.target}
+				style={{
+					left: `${aim?.x}px`,
+					top: `${aim?.y}px`,
+					transform: "translate(-50%, -50%)", // 中心を基準に配置
+				}}
+			>
+				<img
+					src="/2D_material/target.webp"
+					alt="照準の表示"
+					width="100"
+					height="100"
+				/>
+			</div>
 		</div>
 	);
 }
