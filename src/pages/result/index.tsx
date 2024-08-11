b11651039ae4167c33628587f2c2e6d7e1f5737d
Sea10wood <miyusea5@gmail.com>
2024-08-11 11:29:43 +0900

    add; resultPage

--- src/pages/result/index.tsx
+++ src/pages/result/index.tsx
@@ -1,10 +1,51 @@
+import GetImage from "../../components/GetImage";
+import { DefaultButton } from "../../components/ui/Button";
 import styles from "./index.module.css";
 
 function Result() {
+	const images = [
+		"/drink/bottle0.webp",
+		"/drink/bottle1.webp",
+		"/drink/bottle2.webp",
+		"/drink/bottle3.webp",
+	];
+
 	return (
 		<div>
 			<div className={styles["result-text"]}>
 				<p>結果発表!</p>
+			</div>
+			<div className={styles["get-image-container"]}>
+				<GetImage
+					images={images}
+					alt="ランダムに表示されるボトル画像"
+					width={160}
+					height={160} />
+			</div>
+			<div className={styles["background-logo"]}>
+				<img
+					src="/logo.webp"
+					alt="背景にばーちゃるなつまつりのロゴ"
+					width="350"
+					height="350"
+				/>
+			</div>
+			<div className={styles["get-text"]}>
+				<p>Bottle Get!</p>
+			</div>
+			<div className={styles['share-btn']}>
+				<a
+					href="https://twitter.com/intent/tweet?text=Webの射的で遊んだよ.%20%23virtualnatsumatsuri%20%23炎上開発%20%23鹿児島ハッカソン"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<DefaultButton variant="contained" color="black" size="md">
+						Xで共有する
+					</DefaultButton>
+				</a>
+			</div>
+			<div className={styles['replay-text']}>
+				<p>また遊びにきてや〜</p>
 			</div>
 		</div>
 	);
