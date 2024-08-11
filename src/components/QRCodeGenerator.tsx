9ef54aeae47b0cd00bcaf0faa3900f7d493d9c5b
Sea10wood <miyusea5@gmail.com>
2024-08-11 09:00:32 +0900

    fix: 更新されませんでした、、、、

--- src/components/QRCodeGenerator.tsx
+++ src/components/QRCodeGenerator.tsx
@@ -1,22 +1,43 @@
-import QRCode from "qrcode.react";
-import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import { generateUUID } from "../utils/uuid";
+import QRCode from "qrcode.react";
 
 type QRCodeGeneratorProps = {
-	size?: number;
+    size?: number;
+    onUrlGenerated?: (url: string) => void;
 };
 
-const QRCodeGenerator: React.FC<QRCodeGeneratorProps> = ({ size = 128 }) => {
-	const location = useLocation();
-	const baseUrl = `${window.location.origin}${location.pathname}`;
-	const roomId = generateUUID();
-	const url = `${baseUrl}?room_id=${roomId}`;
+const QRCodeGenerator: React.FC<QRCodeGeneratorProps> = ({ size = 128, onUrlGenerated }) => {
+    const location = useLocation();
+    const navigate = useNavigate();
+    const [url, setUrl] = useState<string>("");
 
-	return (
-		<div>
-			<QRCode value={url} size={size} renderAs="svg" />
-		</div>
-	);
+    useEffect(() => {
+        const params = new URLSearchParams(location.search);
+        const existingRoomId = params.get("room_id");
+
+        if (!existingRoomId) {
+            const newRoomId = generateUUID();
+            const newUrl = `${window.location.pathname}?room_id=${newRoomId}`;
+            setUrl(newUrl);
+            onUrlGenerated?.(newUrl);
+
+            if (window.location.href !== newUrl) {
+                navigate(newUrl, { replace: true });
+            }
+        } else {
+            const newUrl = `${window.location.pathname}?room_id=${existingRoomId}`;
+            setUrl(newUrl);
+            onUrlGenerated?.(newUrl);
+        }
+    }, [location.search, navigate, onUrlGenerated]);
+
+    return (
+        <div>
+            <QRCode value={url} size={size} renderAs="svg" />
+        </div>
+    );
 };
 
 export default QRCodeGenerator;
--- src/pages/QRCodeGenerator/index.tsx
+++ src/pages/QRCodeGenerator/index.tsx
@@ -1,11 +1,20 @@
 import QRCodeGenerator from "../../components/QRCodeGenerator";
+import { useState } from "react";
+import QRCode from "qrcode.react";
 
 const QRCodePage: React.FC = () => {
-	return (
-		<div>
-			<QRCodeGenerator size={256} />
-		</div>
-	);
+    const [qrCodeUrl, setQrCodeUrl] = useState<string | null>(null);
+
+    const handleUrlGenerated = (generatedUrl: string) => {
+        setQrCodeUrl(generatedUrl); 
+    };
+
+    return (
+        <div>
+            <QRCodeGenerator size={256} onUrlGenerated={handleUrlGenerated} />
+            {qrCodeUrl && <QRCode value={qrCodeUrl} size={256} renderAs="svg" />}
+        </div>
+    );
 };
 
 export default QRCodePage;
