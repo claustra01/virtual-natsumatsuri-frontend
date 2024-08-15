d0083eb249419256aa8a34dc991fc3bdfd37372a
Sea10wood <miyusea5@gmail.com>
2024-08-15 18:09:23 +0900

    Fix: galleryの変更

--- src/components/Garelly/index.tsx
+++ src/components/Garelly/index.tsx
@@ -3,8 +3,13 @@ import styles from "./index.module.css";
 function Garelly() {
 	return (
 		<div>
-			{/* biome-ignore lint/a11y/useMediaCaption: <explanation> */}
-			<audio src="/sound/natsu_matsuri.mp3" autoPlay loop />
+			{/* biome-ignore lint/a11y/useMediaCaption: 夏祭りの音を再生します。 */}
+			<audio
+				src="/sound/natsu_matsuri.mp3"
+				autoPlay
+				loop
+				aria-label="夏祭りの音"
+			/>
 			<h1 className={styles.title}>VIRTUAL_NATSUMATSURI</h1>
 			<div className={styles.nobori}>
 				<img src="/2D_material/nobori.webp" alt="夏祭り_のぼり" />
