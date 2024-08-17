9c099458e7c83724a2274371ca996c177f38b674
claustra01 <shinya430.g@gmail.com>
2024-08-17 23:59:34 +0900

    chore: target size

--- src/const/target.ts
+++ src/const/target.ts
@@ -12,7 +12,7 @@ export type ThreeDim = {
 
 export const initialTargets: TargetProperty[] = [
 	// 本来posはy=2だが0.1だけ埋め込むことで反動を利用したいい感じのアニメーションを出している
-	{ index: 0, pos: { x: -3, y: 1.9, z: 0 }, size: { x: 0.7, y: 2, z: 0.7 } },
-	{ index: 1, pos: { x: 0, y: 1.9, z: 0 }, size: { x: 0.7, y: 2, z: 0.7 } },
-	{ index: 2, pos: { x: 3, y: 1.9, z: 0 }, size: { x: 0.7, y: 2, z: 0.7 } },
+	{ index: 0, pos: { x: -3, y: 1.7, z: 0 }, size: { x: 0.7, y: 1.8, z: 0.7 } },
+	{ index: 1, pos: { x: 0, y: 1.7, z: 0 }, size: { x: 0.7, y: 1.8, z: 0.7 } },
+	{ index: 2, pos: { x: 3, y: 1.7, z: 0 }, size: { x: 0.7, y: 1.8, z: 0.7 } },
 ];
