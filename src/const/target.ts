871c20576c77325f2f2ea23f072d1a894bd41f98
claustra01 <shinya430.g@gmail.com>
2024-08-17 23:48:16 +0900

    add: initial target properties

---
+++ src/const/target.ts
@@ -0,0 +1,18 @@
+export type TargetProperty = {
+	index: number;
+	pos: ThreeDim;
+	size: ThreeDim;
+};
+
+export type ThreeDim = {
+	x: number;
+	y: number;
+	z: number;
+};
+
+export const initialTargets: TargetProperty[] = [
+	// 本来posはy=2だが0.1だけ埋め込むことで反動を利用したいい感じのアニメーションを出している
+	{ index: 0, pos: { x: -3, y: 1.9, z: 0 }, size: { x: 0.7, y: 2, z: 0.7 } },
+	{ index: 1, pos: { x: 0, y: 1.9, z: 0 }, size: { x: 0.7, y: 2, z: 0.7 } },
+	{ index: 2, pos: { x: 3, y: 1.9, z: 0 }, size: { x: 0.7, y: 2, z: 0.7 } },
+];
