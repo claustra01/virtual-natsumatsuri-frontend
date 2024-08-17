2eaff546a5ba4ca1a5f427f730c3a72aa628d2ff
claustra01 <shinya430.g@gmail.com>
2024-08-18 00:05:07 +0900

    chore: comment fix

--- src/const/target.ts
+++ src/const/target.ts
@@ -11,7 +11,7 @@ export type ThreeDim = {
 };
 
 export const initialTargets: TargetProperty[] = [
-	// 本来posはy=2だが0.1だけ埋め込むことで反動を利用したいい感じのアニメーションを出している
+	// 本来posはy=1.8だが0.1だけ埋め込むことで反動を利用したいい感じのアニメーションを出している
 	{ index: 0, pos: { x: -3, y: 1.7, z: 0 }, size: { x: 0.7, y: 1.8, z: 0.7 } },
 	{ index: 1, pos: { x: 0, y: 1.7, z: 0 }, size: { x: 0.7, y: 1.8, z: 0.7 } },
 	{ index: 2, pos: { x: 3, y: 1.7, z: 0 }, size: { x: 0.7, y: 1.8, z: 0.7 } },
