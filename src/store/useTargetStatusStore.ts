d2eedb8bd5d736e0ed198f4a9738b7b13f24cb9c
claustra01 <shinya430.g@gmail.com>
2024-08-17 23:48:41 +0900

    add: target status store hook

---
+++ src/store/useTargetStatusStore.ts
@@ -0,0 +1,21 @@
+import { create } from "zustand";
+import { TargetStatus } from "../type/target";
+
+type State = {
+	targetStatus: TargetStatus[];
+};
+
+type Action = {
+	updateTargetStatus: (index: number, status: TargetStatus) => void;
+};
+
+export const useTargetStatusStore = create<State & Action>((set) => ({
+	// left, center, right
+	targetStatus: [TargetStatus.Live, TargetStatus.Live, TargetStatus.Live],
+	updateTargetStatus: (index: number, status: TargetStatus) =>
+		set((state) => {
+			const targetStatus = [...state.targetStatus];
+			targetStatus[index] = status;
+			return { targetStatus };
+		}),
+}));
